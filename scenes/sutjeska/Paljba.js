db8bf0d03c928bd03f10ce4d1a8b8bde0519f755
Damjan Pavlica <d_pavlica@wargaming.net>
2025-12-22 16:13:29 +0100

    SutjeskaScena: dodati zvuk na granate

--- scenes/sutjeska/Paljba.js
+++ scenes/sutjeska/Paljba.js
@@ -6,6 +6,9 @@ export default class Paljba extends Predmet {
   constructor() {
     super('items/krateri/krater.gif')
     this.postaviRandom()
+    const zvuk = new Audio('/assets/sounds/explosion.mp3')
+    zvuk.volume = Math.random()
+    zvuk.play()
   }
 
   randomX(marginaX) {
