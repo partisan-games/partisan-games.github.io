7b94e31df6192c808b587ede071fd2e23867f882
Damjan Pavlica <mudroljub@gmail.com>
2026-02-08 19:13:26 +0100

    m

--- scenes/moon-landing/Main.js
+++ scenes/moon-landing/Main.js
@@ -27,32 +27,26 @@ export default class extends Scena3D {
 
   async init() {
     // TODO: boja pozadine
-    document.body.style.background = 'linear-gradient(to bottom, #020111 70%, #191621 100%)'
+    // document.body.style.background = 'linear-gradient(to bottom, #020111 70%, #191621 100%)'
     this.camera.position.z = 18
 
     ambLight({ scene: this.scene })
     const moonLight = createMoon({ pos: [30, 30, 30], intensity: Math.PI * .2 })
-
     const platform = new Platform({ pos: [0, -10, 0], axis: 'x', range: 29, randomDirChange: true })
-    this.stars = new Stars({ minRadius: 150 })
+    const stars = new Stars({ minRadius: 150, minVelocity: 0.25, maxVelocity: 1.5 })
 
     this.jupiter = createJupiter({ r: 5 })
     this.jupiter.position.set(-125, 25, -80)
-
     this.moon = createSphere({ r: 1.5, file: 'planets/moon.jpg' })
-
     this.saturn = createSaturn({ r: 3 })
     this.saturn.position.set(85, 20, -50)
 
-    this.addMesh(this.stars.mesh, this.jupiter, this.saturn, this.moon, moonLight)
-    this.add(platform)
-
     this.arcology = await loadModel({ file: 'space/arcology-ring/model.fbx', scale: .5, shouldCenter: true })
     this.arcology.position.z = -100
-    this.addMesh(this.arcology)
+    this.addMesh(this.jupiter, this.saturn, this.moon, moonLight, this.arcology)
 
     this.lander = new Lander({ platform })
-    this.addMesh(this.lander.mesh)
+    this.add(this.lander, stars, platform)
   }
 
   reset() {
@@ -63,23 +57,18 @@ export default class extends Scena3D {
   /* LOOP */
 
   sceneUI() {
-    if (!this.lander) return ''
     const points = this.lander.hasLanded ? this.lander.fuel : 0
     return /* html */`
       <div class="top-left">
         Score: ${points}
-        <br><small class="">Fuel left: ${this.lander.fuel}</small>
+        <br><small>Fuel left: ${this.lander.fuel}</small>
       </div>
     `
   }
 
   update(dt, time) {
     super.update(dt)
-    if (!this.lander) return
 
-    this.lander.update(dt)
-
-    this.stars.update({ delta: dt * .005 })
     this.arcology?.rotateY(dt * .02)
     this.jupiter.rotateY(dt * .2)
     this.moon.rotateY(dt)
