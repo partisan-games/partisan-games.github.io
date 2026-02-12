02c6f6b2915ba189fbfb258e433a0bc04696dbae
Damjan Pavlica <mudroljub@gmail.com>
2026-02-12 20:46:10 +0100

    refactor platno

--- core/Scena.js
+++ core/Scena.js
@@ -1,12 +1,11 @@
 import { keyboard } from './io/Keyboard.js'
-import { platno } from './io/platno.js'
 import GameLoop from './GameLoop.js'
 import UI from '../ui/UI.js'
 import Controls from '../ui/Controls.js'
 
 export default class Scena {
   constructor({
-    usePointerLock, controlKeys, intro, reportText, customStartScreen, startButtonText, showControls = true, uiStyle
+    usePointerLock, controlKeys, intro, reportText, customStartScreen, startButtonText, showControls = true, uiStyle, canvas
   } = {}) {
     this.usePointerLock = usePointerLock
     this.uiStyle = uiStyle
@@ -16,6 +15,10 @@ export default class Scena {
     if (showControls)
       this.controlsUI = new Controls({ controlKeys, uiStyle })
 
+    this.canvas = canvas
+    this.canvas.style.display = 'block'
+
+    /* EVENTS */
     this.handleClick = this.handleClick.bind(this)
     this.handlePointerLockChange = this.handlePointerLockChange.bind(this)
     this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
@@ -40,11 +43,11 @@ export default class Scena {
   /* GETTERS */
 
   get sirina() {
-    return platno.width
+    return this.canvas.width
   }
 
   get visina() {
-    return platno.height
+    return this.canvas.height
   }
 
   get hasStartScreen() {
@@ -102,6 +105,8 @@ export default class Scena {
     if (this.controlsUI) this.controlsUI.end()
     if (this.controls2UI) this.controls2UI.end()
     if (this.player) this.player.end()
+
+    this.canvas.style.display = 'none'
 
     document.removeEventListener('click', this.handleClick)
     document.removeEventListener('visibilitychange', this.handleVisibilityChange)
--- core/Scena3D.js
+++ core/Scena3D.js
@@ -2,22 +2,20 @@ import * as THREE from 'three'
 import Scena from './Scena.js'
 
 const platno3D = document.getElementById('platno-3d')
-// platno3D.style.display = 'none'
 
 export default class Scena3D extends Scena {
-  constructor({ toon = false, ...rest } = {}) {
-    super({ ...rest })
+  constructor({ canvas = platno3D, toon = false, ...rest } = {}) {
+    super({ canvas, ...rest })
     this.scene = new THREE.Scene()
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     this.camera.position.set(0, 5, 30)
 
-    this.renderer = new THREE.WebGLRenderer({ canvas: platno3D, alpha: true, antialias: true })
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas, alpha: true, antialias: true })
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     this.renderer.setPixelRatio(Math.min(2, window.devicePixelRatio)) // save battery by limit pixel ratio to 2
     this.renderer.shadowMap.enabled = true
     if (toon) this.createToonRenderer()
 
-    platno3D.style.display = 'block'
     this.bojaPlatna = 'linear-gradient(180deg, #0B3D91 0%, #000000 100%)'
 
     window.addEventListener('resize', () => {
@@ -32,7 +30,7 @@ export default class Scena3D extends Scena {
   }
 
   set bojaPlatna(boja) {
-    platno3D.style.background = boja
+    this.canvas.style.background = boja
   }
 
   async createToonRenderer(defaultThickness = 0.0025) {
@@ -57,11 +55,6 @@ export default class Scena3D extends Scena {
 
   removeMesh(...meshes) {
     this.scene.remove(...meshes)
-  }
-
-  end() {
-    super.end()
-    platno3D.style.display = 'none'
   }
 
   handleInput(dt) {
