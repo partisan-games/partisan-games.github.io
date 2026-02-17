1876bac61375b6195bfbb9cda51eb1c89b4818f3
Damjan Pavlica <mudroljub@gmail.com>
2026-02-17 23:26:16 +0100

    - bunker: mitraljez z povećati

--- scenes/bunker/Mitraljezac.js
+++ scenes/bunker/Mitraljezac.js
@@ -3,7 +3,7 @@ import { praviPucanje } from '/core/actor/prosirenja/pucanje.js'
 
 export default class Mitraljezac extends Predmet {
   constructor(x, y, cilj) {
-    super('armies/nemci/mitraljezac-01.png', { x, y, ishodiste: 'DOLE_DESNO' })
+    super('armies/nemci/mitraljezac-01.png', { x, y, z: -1, ishodiste: 'DOLE_DESNO' })
     const autoPucanje = praviPucanje({
       stankaPucanja: 3, src: 'items/granata.gif', skalar: .4, potisakMetka: 600, y: -10,
     })
